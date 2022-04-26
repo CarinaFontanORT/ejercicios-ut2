13039727edcd50a78950cfda73c65a51031b6858
Alejo Pereira <alejo.pereira@innovant.us>
2022-04-25 21:06:47 -0300

    Create MS Test Project

---
+++ UnitTestPracticoUT2/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestPracticoUT2")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestPracticoUT2")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("fcc57e16-91f2-44f3-890c-578a08c214b4")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTestPracticoUT2/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestPracticoUT2
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void FirstTest()
+        {
+            int randomNumber = 0;
+
+            Assert.AreEqual(0, randomNumber);
+        }
+    }
+}
